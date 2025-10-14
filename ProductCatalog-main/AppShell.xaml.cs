5a87da48f36ab8eb579e5e5f7ec1fb93e096b9d2
DiegoRT14 <100293782+DiegoRT14@users.noreply.github.com>
2025-10-14 17:59:47 -0600

    Prueba

--- ProductCatalog-main/AppShell.xaml.cs
+++ ProductCatalog-main/AppShell.xaml.cs
@@ -5,6 +5,18 @@
         public AppShell()
         {
             InitializeComponent();
+
+            Routing.RegisterRoute(nameof(Pages.ProductListPage), typeof(Pages.ProductListPage));
+            Routing.RegisterRoute(nameof(Pages.ProductEditPage), typeof(Pages.ProductEditPage));
+
+            Routing.RegisterRoute(nameof(Pages.CategoryListPage), typeof(Pages.CategoryListPage));
+            Routing.RegisterRoute(nameof(Pages.CategoryEditPage), typeof(Pages.CategoryEditPage));
+
+            Routing.RegisterRoute(nameof(Pages.SupplierListPage), typeof(Pages.SupplierListPage));
+            Routing.RegisterRoute(nameof(Pages.SupplierEditPage), typeof(Pages.SupplierEditPage));
+
+            Routing.RegisterRoute(nameof(Pages.BrandListPage), typeof(Pages.BrandListPage));
+            Routing.RegisterRoute(nameof(Pages.BrandEditPage), typeof(Pages.BrandEditPage));
         }
     }
 }
